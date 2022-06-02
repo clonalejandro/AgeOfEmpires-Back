f645787de5a4718380b72d43816981f61cb3d309
clonalejandro <clonalejandromcb@gmail.com>
2022-06-02 19:25:19 +0200

    - added user endpoints

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,12 +1,16 @@
+import { query } from '../utils/sql'
+import encrypt from '../utils/encrypt'
+
 export default [
     {
         id: 'user',
-        type: 'get',
-        callback: (req: any, res: any) => {},
-    },
-    {
-        id: 'user',
         type: 'post',
-        callback: (req: any, res: any) => {},
+        callback: (req: any, res: any) => {
+            const { username, password }: IUser = req.body
+
+            query('INSERT INTO users (username, password) VALUES (?, ?)', [username, encrypt(password)])
+                .then((row) => res.status(200).send(row))
+                .catch((err) => res.status(500).send(err))
+        },
     },
 ]
